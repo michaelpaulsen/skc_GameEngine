e9801f394b44a97e822a9cc9f8f8c6a502f07fec
Michael <Michael.J.Paulsen@GMail.com>
2022-05-13 11:54:18 -0700

    added GetFirstComponetByName to Entity.h GetFirstComponetByName(std::string) takes the given string and loops though the vector of components until it finds a component with the same name. If no component is found returns nullptr.

--- headers/Entity.h
+++ headers/Entity.h
@@ -3,14 +3,14 @@
 #include <vector>
 #include <string>
 #include <stdarg.h>
-#include ".\Component.h"
+#include "Component.h"
 
 class Entity {
 public: 
 	std::vector<Component*> components;
-	Component* GetFirstComponetByName() {
-		for (auto &x : this->components) {
-			if (x.name = name) return x; 
+	Component* GetFirstComponetByName(std::string name) {
+		for (const auto &x : this->components) {
+			if (x->name == name) return x; 
 		}
 	}
 };
