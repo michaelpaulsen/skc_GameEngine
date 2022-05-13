2984c3c5b677e25c22103ff9dbd4445aacf1d600
Michael <Michael.J.Paulsen@GMail.com>
2022-05-13 15:12:46 -0700

    System.h added CreateEntity(int) it creates an Entity of the type(int) passed in and pushes it into the entity vector. returns the UUID of the Entity created;

--- headers/System.h
+++ headers/System.h
@@ -1,10 +1,21 @@
 #ifndef ECS
 #define ECS 
-#include "Entity.h"
 #include <vector>
+#include <stdint.h>
+//not nessiary for msvs but put it in here so that other compilers have it 
+///custom files
+#include "Entity.h"
 class System {
 	std::vector<Entity*> entities;
 	/// this will hold other things like the renderer and input systems but for now it just holds the suff for the entity
+	uint64_t CreateEntity(int type = 0) {
+		/// this should be from your entity_type struct*/
+		/// this should be the main way that you create entities if you want to keep the uuid 
+		auto e = Entity(type);
+		auto GUID = e.GetUUID(); 
+		this->entities.push_back(&e);
+		return GUID; 
+	}
 	Entity* GetEntityByUUID(uint64_t uuid) {
 		for (const auto &x : this->entities) {
 			if (x->GetUUID() == uuid)return x;
@@ -16,14 +27,14 @@ class System {
 		std::vector< Entity*> ent;
 		for (auto& x : this->entities) {
 			if (x->GetType() == type) {
-				ent.push_back(&x);
+				ent.push_back(x);
 			}
 		}
 		return ent;
 		///this should be the list of all of the entities of a given type. 
 	}
 	void AddEntity(Entity* ent) {
-		entities.push_back(ent);x
+		entities.push_back(ent);
 	}
 };
 #endif // !ECS
