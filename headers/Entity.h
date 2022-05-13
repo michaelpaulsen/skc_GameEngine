bad5088bb10bd700038625c86a429a4ec33d7047
Michael <Michael.J.Paulsen@GMail.com>
2022-05-13 12:58:21 -0700

    moved uuid & type to be private and added comments; added functions to access them.

--- headers/Entity.h
+++ headers/Entity.h
@@ -10,13 +10,19 @@
 
 class Entity {
 	static uint64_t nextuuid = 0;
+	       uint64_t uuid;
+	       uint64_t type;
+	///this is the type of the entity
+	///the end user should define an enum that has all of the types that they use
+	///this can be used by the system to return a vector of all of the entities of that type
 public: 
-	uint64_t uuid; 
-	Entity() {
-		this->uuid = nextuuid++;
+	Entity(int t = 0) : uuid(nextuuid++), type(t) {
 		///nothing should decrement nextuuid 
-		/// it only would add overhead to the run-time to add the uuid 
+		/// it only would add overhead to the run-time to add the uuid using the os dependent function calls
 		/// + that isn't cross operating system.
+	}
+	auto GetType() {
+		return this->type; 
 	}
 	auto GetUUID() {
 		return this->uuid; 
--- headers/System.h
+++ headers/System.h
@@ -7,11 +7,10 @@ class System {
 	/// this will hold other things like the renderer and input systems but for now it just holds the suff for the entity
 	Entity* GetEntityByUUID(uint64_t uuid) {
 		for (const auto &x : this->entities) {
-			if (x->uuid == uuid)return x; 
-			///it is up to the codder to keep track of the uuids. 
+			if (x->GetUUID() == uuid)return x;
+			///it is up to the end-user to keep track of the uuids. 
 			/// they might want to put the uuid for the player(s) in thier own vars... 
 		}
 	}
-
 };
 #endif // !ECS
