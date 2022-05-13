7100de36b89704aa118f8c06927284542386c0a7
Michael <Michael.J.Paulsen@GMail.com>
2022-05-13 12:53:42 -0700

    added basic Entity code to System. at this point it just stores a vector of Entities and allows you to get them with GetEntityByUUID.

--- headers/System.h
+++ headers/System.h
@@ -1,4 +1,17 @@
 #ifndef ECS
 #define ECS 
-#include "Entity.h";
+#include "Entity.h"
+#include <vector>
+class System {
+	std::vector<Entity*> entities;
+	/// this will hold other things like the renderer and input systems but for now it just holds the suff for the entity
+	Entity* GetEntityByUUID(uint64_t uuid) {
+		for (const auto &x : this->entities) {
+			if (x->uuid == uuid)return x; 
+			///it is up to the codder to keep track of the uuids. 
+			/// they might want to put the uuid for the player(s) in thier own vars... 
+		}
+	}
+
+};
 #endif // !ECS
