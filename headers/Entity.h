f9b06bfc9b1253e4a6786a2a4ab1885fd010bffb
Michael <Michael.J.Paulsen@GMail.com>
2022-05-13 12:22:20 -0700

    added a uuid system to the Entity class. this allows you to get the Entity by its uuid didn't use name because not all Entities have a name[namely Entities that are invisible].

--- headers/Entity.h
+++ headers/Entity.h
@@ -3,10 +3,24 @@
 #include <vector>
 #include <string>
 #include <stdarg.h>
+#include <stdint.h>
+//not nessiary for msvs but put it in here so that other compilers have it 
+/// custom files. 
 #include "Component.h"
 
 class Entity {
+	static uint64_t nextuuid = 0;
 public: 
+	uint64_t uuid; 
+	Entity() {
+		this->uuid = nextuuid++;
+		///nothing should decrement nextuuid 
+		/// it only would add overhead to the run-time to add the uuid 
+		/// + that isn't cross operating system.
+	}
+	auto GetUUID() {
+		return this->uuid; 
+	}
 	std::vector<Component*> components;
 	Component* GetFirstComponetByName(std::string name) {
 		for (const auto &x : this->components) {
