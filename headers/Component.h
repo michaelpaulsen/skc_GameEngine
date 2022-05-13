a86a1e0c93f7753c068692b4992102a286c234ae
Michael <Michael.J.Paulsen@GMail.com>
2022-05-13 12:55:05 -0700

    added comments to Component.h

--- headers/Component.h
+++ headers/Component.h
@@ -17,9 +17,14 @@ public:
 	virtual bool IsActive() {
 		return this->active; 
 	}
+	
 	virtual int OnActivate() {
 		return EXIT_SUCESS;
 	}
+	///I'm shure im not the first one to do this but I'm implementing it in my own way
+	/// what making this a variadic function does is it allows you to pass in any function
+	/// then in the override of run only deal with the ones that you want to
+	/// this prevents you from having to pass a whole GameState object to the run function
 	virtual int Run(...) {
 		return EXIT_SUCESS;
 	}
